3e61812530e289c6416d80c1f3ff30760fe5f58b
Connor McCarl <technicians@novatechnologies.us>
2018-12-12 19:24:21 -0700

    Updated jwt exp redirection to home page

--- starter/client/index.js
+++ starter/client/index.js
@@ -13,15 +13,17 @@ import jwt_decode from "jwt-decode";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+// Set store.auth if the user returns after leaving the page OR reloads the page
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
   const decoded = jwt_decode(localStorage.jwtToken);
   store.dispatch(setCurrentUser(decoded));
 
+  // If the jwt has expired, log the user out and redirect to the home page
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
-    window.location.href = "/login";
+    window.location.href = "/";
   }
 }
 
